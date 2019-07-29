3df1370c8669c85116ac53d0e2d653814e041ecc
Yuliya <harmful.sun@gmail.com>
2019-07-29 16:26:55 +0300

    feat: add timeline component

---
+++ src/components/authortimeline/AuthorTimeline.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
+
+const inputdata = [
+  {
+    date: '1982',
+    description: 'Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla',
+  },
+  {
+    date: '1985',
+    description: 'Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla',
+  },
+  {
+    date: '1992',
+    description: 'Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla',
+  },
+  {
+    date: '2000',
+    description: 'Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla',
+  },
+]
+
+const AuthorTimeline = () => (
+  <Timeline lineColor="#ddd">
+    {inputdata.map(item => (
+      <TimelineItem
+        key={item.date}
+        dateText={item.date}
+        style={{ color: '#e86971' }}
+      >
+        <p>{item.description}</p>
+      </TimelineItem>
+    ))}
+  </Timeline>
+);
+
+export default AuthorTimeline;
