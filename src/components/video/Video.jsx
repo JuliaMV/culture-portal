94837c52e0cad30f605763e0f587e77a281f819b
Yuliya <harmful.sun@gmail.com>
2019-07-30 22:38:52 +0300

    feat: add basic video component

---
+++ src/components/video/Video.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'react-responsive-modal';
+
+import styles from './Video.module.scss';
+
+class Video extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
+  showVideo = () => {
+    this.setState({
+      isOpen: true,
+    });
+  }
+
+  hideVideo = () => {
+    this.setState({
+      isOpen: false,
+    });
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    const { url } = this.props;
+    return (
+      <div>
+        <button type="button" onClick={this.showVideo}>
+          Show Video
+        </button>
+        <Modal
+          open={isOpen}
+          onClose={this.hideVideo}
+          center
+          styles={{
+            modal: {
+              padding: 0,
+              bacgkroundColor: '#000000',
+            },
+          }}
+        >
+          <iframe
+            className={styles.VideoFrame}
+            title="video"
+            width="560"
+            height="315"
+            src={url}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+
+Video.propTypes = {
+  url: PropTypes.string.isRequired,
+};
+
+export default Video;
