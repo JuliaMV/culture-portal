3abea62568cece2c342602649a181f93c5fcd1de
Hanna Klempach <klempach.teaching@gmail.com>
2019-08-01 02:09:48 +0300

    feat: add draft display to the list of authors page

--- src/pages/artists.en.jsx
+++ src/pages/artists.en.jsx
@@ -1,47 +1,27 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import Layout from '../components/layout/Layout';
 
 const Item = queryData => (
   <div>
     <img src={queryData.node.personalPhoto.file.url} alt={queryData.node.personalPhoto.title} width="140" height="170" />
-    <h2>{`${queryData.node.name.name} ${queryData.node.patronymic.patronymic} ${queryData.node.surname.surname}`}</h2>
+    <h2>
+      <Link to={`en/artists/${queryData.node.slug}`}>
+        {`${queryData.node.name.name} ${queryData.node.patronymic.patronymic} ${queryData.node.surname.surname}`}
+      </Link>
+    </h2>
     <p>{queryData.node.yearsOfLife}</p>
-    <a href={queryData.node.videoTag.videoTag} target="_blank" rel="noopener noreferrer">Youtube Video</a>
   </div>
 );
 
 const IndexPage = ({ data, location }) => {
   const items = data.allContentfulArchitectPage.edges.map(edge => Item(edge));
-  const imagesList = data.allContentfulArchitectPage.edges.map((edge) => {
-    const images = edge.node.photoGallery.map(currImg => (
-      <li>
-        <img src={currImg.file.url} alt={currImg.title} width="50" height="50" />
-      </li>
-    ));
-    return (
-      <ul>
-        {images}
-      </ul>
-    );
-  });
-  const timeline = data.allContentfulTimeline.edges.map(edge => (
-    <li>
-      <p>
-        {`${edge.node.date} - ${edge.node.description.description}`}
-      </p>
-    </li>
-  ));
   return (
     <Layout data={data} location={location}>
       <ul>
         {items}
       </ul>
-      <ul>
-        {timeline}
-      </ul>
-      {imagesList}
     </Layout>
   );
 };
