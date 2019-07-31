9088cc3fd815567bf47413963445a80076703bfb
Hanna Klempach <klempach.teaching@gmail.com>
2019-07-31 23:52:05 +0300

    feat: add multilanguage display for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-
+import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
-
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { FormattedMessage } from 'react-intl';
 import { withStyles } from '@material-ui/core/styles';
 import { grey } from '@material-ui/core/colors';
-
-// import SelectLanguage from '../SelectLanguage';
+import SelectLanguage from '../selectlanguage/SelectLanguage';
 // @ts-ignore
 import headerStyles from './Header.module.scss';
 
@@ -26,30 +24,32 @@ const ColorButton = withStyles(theme => ({
   },
 }))(Button);
 
-//  header props was { langs }
-const Header = () => (
+
+// you can change the labels for buttons in 'data/messages' en.js, ru.js, be.js accordingly
+const Header = ({ langs, url }) => (
   <header className={headerStyles.header}>
     <div className={headerStyles.headerWrapper}>
       <Container>
-        <Link to="/" className={headerStyles.logoLink}>
+        <Link to={`${url}`} className={headerStyles.logoLink}>
           <h1 className={headerStyles.headerTextLogo}>Architects of Belarus</h1>
         </Link>
-        {/* <SelectLanguage langs={langs} />  !Anna Klempach it's tag for button translations */}
+        <SelectLanguage langs={langs} />
       </Container>
       <ButtonGroup fullWidth>
-        <ColorButton variant="contained" href="/">
-            Main
+        <ColorButton variant="contained" href={`${url}`}>
+          <FormattedMessage id="mainPageButton" />
         </ColorButton>
-        <ColorButton variant="contained" href="/list-of-architecture">
-            List of architecture
+        <ColorButton variant="contained" href={`${url}/artists`}>
+          <FormattedMessage id="artistsPageButton" />
         </ColorButton>
       </ButtonGroup>
     </div>
   </header>
 );
 
-// Header.propTypes = {
-//   langs: PropTypes.string.isRequired,
-// };
+Header.propTypes = {
+  langs: PropTypes.objectOf(PropTypes.string).isRequired,
+  url: PropTypes.string.isRequired,
+};
 
 export default Header;
