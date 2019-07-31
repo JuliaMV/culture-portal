b09b3f8b8b78a67c4b6a0e20c82fd651d7c0ee00
Anna-Pro <you@example.com>
2019-07-31 15:17:50 +0300

    feat: add header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,37 +1,52 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
+
 import Link from 'gatsby-link';
-import PropTypes from 'prop-types';
 
-import SelectLanguage from '../SelectLanguage';
-import styles from './Header.module.scss';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { withStyles } from '@material-ui/core/styles';
+import { grey } from '@material-ui/core/colors';
 
-const Header = ({ langs }) => (
-  <div className={styles.Header}>
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-      <SelectLanguage langs={langs} />
-    </div>
-  </div>
+// import SelectLanguage from '../SelectLanguage';
+import headerStyles from './Header.module.scss';
+
+const ColorButton = withStyles(theme => ({
+  root: {
+    height: 48,
+    color: theme.palette.getContrastText(grey[900]),
+    backgroundColor: grey[900],
+    '&:hover': {
+      backgroundColor: grey[700],
+    },
+
+    boxShadow: '0px -6px 13px 2px rgb(55, 71, 79)',
+  },
+}))(Button);
+
+//  header props was { langs }
+const Header = () => (
+  <header className={headerStyles.header}>
+    <Container>
+      <Link to="/" className={headerStyles.logoLink}>
+        <h1 className={headerStyles.headerTextLogo}>Architects of Belarus</h1>
+      </Link>
+      {/* <SelectLanguage langs={langs} />  !Anna Klempach it's tag for button translations */}
+    </Container>
+    <ButtonGroup fullWidth>
+      <ColorButton variant="contained" href="/">
+          Main
+      </ColorButton>
+      <ColorButton variant="contained" href="/list-of-architecture">
+          List of architecture
+      </ColorButton>
+    </ButtonGroup>
+  </header>
 );
 
-Header.propTypes = {
-  langs: PropTypes.string.isRequired,
-};
+// Header.propTypes = {
+//   langs: PropTypes.string.isRequired,
+// };
 
 export default Header;
