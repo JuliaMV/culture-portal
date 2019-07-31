855feec2e68e218a3f020ec333463f6ec4bb70ba
Hanna Klempach <klempach.teaching@gmail.com>
2019-08-01 01:51:10 +0300

    feat: implement video and timeline elements

--- src/templates/artist-page.jsx
+++ src/templates/artist-page.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import * as PropTypes from 'prop-types';
 // import { Link, graphql } from 'gatsby';
 import { graphql } from 'gatsby';
+import { FormattedMessage } from 'react-intl';
 // import Img from 'gatsby-image';
 // import { rhythm } from '../utils/typography';
-
+import Video from '../components/video/Video';
 import Layout from '../components/layout/Layout';
 import ArtistTimeline from '../components/artisttimeline/ArtistTimeline';
 
-const artistPageTemplate = ({ data, location }) =>
-  // const artist = data.contentfulProduct;
-  // const {
-  //   productName: { productName },
-  //   productDescription,
-  //   price,
-  //   image,
-  //   brand,
-  //   categories,
-  // } = artist;
-  (// eslint-disable-line
+const artistPageTemplate = ({ data, location }) => {
+  const artist = data.contentfulArchitectPage;
+  const {
+    videoTag: { videoTag },
+    surname: { surname },
+    name: { name },
+    patronymic: { patronymic },
+  } = artist;
+  return (// eslint-disable-line
     <Layout data={data} location={location}>
       <main className="artist-page">
         <div className="wrapper">
@@ -26,7 +25,7 @@ const artistPageTemplate = ({ data, location }) =>
             <div className="artist__img">
               {/* <Img resolutions={image[0].resolutions} /> */}
             </div>
-            <h1>artist Name</h1>
+            <h1>{`${surname} ${name} ${patronymic}`}</h1>
             <span className="artist__date">(99 декабря 9999 - 99 декабря 9999)</span>
             <p className="artist__description">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
@@ -34,29 +33,40 @@ const artistPageTemplate = ({ data, location }) =>
             </p>
           </section>
           <section className="artist__timeline">
-            <h2>Временная линия</h2>
-            <ArtistTimeline />
+            <h2>
+              <FormattedMessage id="timelineTitle" />
+            </h2>
+            <ArtistTimeline inputData={data.allContentfulTimeline.edges} />
           </section>
           <section className="artist__buildings">
-            <h2>Здания</h2>
+            <h2>
+              <FormattedMessage id="worksTitle" />
+            </h2>
             {/* <Buildings></Buildings> */}
           </section>
           <section className="artist__video">
-            <h2>Видео</h2>
-            {/* <Video></Video> */}
+            <h2>
+              <FormattedMessage id="videoTitle" />
+            </h2>
+            <Video url={videoTag} />
           </section>
           <section className="artist__map">
-            <h2>Карта</h2>
+            <h2>
+              <FormattedMessage id="mapTitle" />
+            </h2>
             {/* <Map></Map> */}
           </section>
           <section className="gallery">
-            <h2>Галлерея</h2>
+            <h2>
+              <FormattedMessage id="galleryTitle" />
+            </h2>
             {/* <Gallery></Gallery> */}
           </section>
         </div>
       </main>
     </Layout>
   );
+};
 artistPageTemplate.propTypes = {
   data: PropTypes.object.isRequired, // eslint-disable-line
   location: PropTypes.object.isRequired, // eslint-disable-line
@@ -65,7 +75,7 @@ artistPageTemplate.propTypes = {
 export default artistPageTemplate;
 
 export const pageQuery = graphql`
-    query productQuery($id: String!) {
+    query artistQuery($slug: String!, $lang: String!) {
         site {
             siteMetadata {
                 languages {
@@ -74,38 +84,44 @@ export const pageQuery = graphql`
                 }
             }
         }
-        contentfulProduct(id: { eq: $id }) {
-            productName {
-                productName
+        allContentfulTimeline(filter: {lang: {eq: $lang}}, sort: {fields: order}) {
+          edges {
+            node {
+              date
+              description {
+                description
+              }
+              order
             }
-            productDescription {
-                childMarkdownRemark {
-                    html
-                }
-            }
-            price
-            image {
-                resolutions(width: 50, height: 50) {
-                    base64
-                    src
-                    srcSet
-                    height
-                    width
-                }
-            }
-            brand {
-                companyName {
-                    companyName
-                }
-            }
-            categories {
-                id
-                contentful_id
-                node_locale
-                title {
-                    title
-                }
-            }
+          }
+        }
+        contentfulArchitectPage(slug: { eq: $slug }, lang: { eq: $lang },) {
+            slug
+        patronymic {
+          patronymic
+        }
+        name {
+          name
+        }
+        personalPhoto {
+          file {
+            url
+            fileName
+          }
+        }
+        photoGallery {
+          file {
+            url
+          }
+          title
+        }
+        surname {
+          surname
+        }
+        videoTag {
+          videoTag
+        }
+        yearsOfLife
         }
     }
 `;
