2828354652b9b58ccb864dd90c1468f065e61df2
Hanna Klempach <klempach.teaching@gmail.com>
2019-08-01 10:38:50 +0300

    feat: add error handling on creating artist pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,37 @@
 const path = require('path');
 
-module.exports.createPages = async ({ graphql, actions }) => {
+module.exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
-  const artistTemplate = path.resolve('./src/templates/artist-page.jsx');
-  const res = await graphql(`
-    query {
-      allContentfulArchitectPage {
-          edges {
-            node {
-              slug
-              lang
+  return new Promise((resolve, reject) => {
+    const artistTemplate = path.resolve('./src/templates/artist-page.jsx');
+    resolve(
+      graphql(
+        `query {
+          allContentfulArchitectPage {
+              edges {
+                node {
+                  slug
+                  lang
+                }
+              }
             }
           }
+        `,
+      ).then((result) => {
+        if (result.errors) {
+          reject(result.errors);
         }
-      }
-    `);
-  res.data.allContentfulArchitectPage.edges.forEach((edge) => {
-    createPage({
-      component: artistTemplate,
-      path: `${edge.node.lang}/artists/${edge.node.slug}`,
-      context: {
-        lang: edge.node.lang,
-        slug: edge.node.slug,
-      },
-    });
+        result.data.allContentfulArchitectPage.edges.forEach((edge) => {
+          createPage({
+            component: artistTemplate,
+            path: `${edge.node.lang}/artists/${edge.node.slug}`,
+            context: {
+              lang: edge.node.lang,
+              slug: edge.node.slug,
+            },
+          });
+        });
+      }),
+    );
   });
 };
