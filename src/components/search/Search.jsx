eb791829da478ee6bb37fabc9378899d2b6478ed
pryvvid <pryvvid@gmail.com>
2019-08-02 22:02:31 +0300

    feat: add search component

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+const architectsList = [
+  {
+    id: 1,
+    name: 'John',
+    surname: 'Doe',
+    city: 'Minsk',
+  },
+  {
+    id: 2,
+    name: 'Mike',
+    surname: 'Hike',
+    city: 'Hrodno',
+  },
+  {
+    id: 3,
+    name: 'Jane',
+    surname: 'Doe',
+    city: 'Vitebsk',
+  },
+  {
+    id: 4,
+    name: 'Sarah',
+    surname: 'Smith',
+    city: 'Homel',
+  },
+
+];
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      architects: architectsList,
+      query: '',
+    };
+  }
+
+  searchHandler(event) {
+    this.setState({ query: event.target.value });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  searchFor(query) {
+    // eslint-disable-next-line func-names
+    return function (x) {
+      return x.name.toLowerCase().includes(query.toLowerCase())
+             || x.surname.toLowerCase().includes(query.toLowerCase())
+             || x.city.toLowerCase().includes(query.toLowerCase()) || !query;
+    };
+  }
+
+  render() {
+    const { architects, query } = this.state;
+    return (
+      <div className="App">
+        <form>
+          <input
+            type="text"
+            value={query}
+            onChange={this.searchHandler.bind(this)}
+          />
+        </form>
+        <ul className="architects-list">
+          {
+          architects.filter(this.searchFor(query)).map(a => (
+            <li
+              className="architects-list-item"
+              key={a.id}
+            >
+              {a.name}
+              {' '}
+              {a.surname}
+,
+              {' '}
+              {a.city}
+            </li>
+          ))
+        }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Search;
