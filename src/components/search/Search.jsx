50f20a2a2708a5375ee5045664f1931f902157b3
pryvvid <pryvvid@gmail.com>
2019-08-03 18:54:09 +0300

    feat: add styles to search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ArtistCard from '../artistcard/ArtistCard';
+import searchStyles from './Search.module.scss';
 
 class Search extends Component {
   static searchFor(edge, query) {
@@ -25,18 +26,43 @@ class Search extends Component {
     const { searchData } = this.props;
     const searchItems = searchData.filter(edge => Search.searchFor(edge, searchQuery));
     const searchResultItems = searchItems ? searchItems.map(edge => ArtistCard(edge)) : null;
+    const searchLanguage = searchData.map(edge => edge.node.lang);
+    const currentLanguage = searchLanguage[0];
+
+    let placeholderText;
+    let noResultsMessage;
+
+    switch (currentLanguage) {
+      case 'ru':
+        placeholderText = 'Поиск';
+        noResultsMessage = 'Ничего не найдено';
+        break;
+      case 'be':
+        placeholderText = 'Пошук';
+        noResultsMessage = 'Нічога не знойдзена';
+        break;
+      case 'en':
+        placeholderText = 'Search';
+        noResultsMessage = 'No results found';
+        break;
+      default:
+        break;
+    }
+
     return (
-      <div>
+      <div className={searchStyles.artistsWrapper}>
         <form>
           <input
+            className={searchStyles.searchInput}
             type="text"
             value={query}
+            placeholder={placeholderText}
             onChange={this.searchHandler}
           />
         </form>
         {searchResultItems.length > 0
-          ? <ul className="artists-list">{searchResultItems}</ul>
-          : <p>No results found</p>}
+          ? <ul className={searchStyles.artistsList}>{searchResultItems}</ul>
+          : <p className={searchStyles.errorMessage}>{noResultsMessage}</p>}
       </div>
     );
   }
